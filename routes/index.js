574125d8bbfb1508f8ca9638d3ae3228b74e258b
Gabe Goodhart <ghart@us.ibm.com>
2019-01-22 15:55:10 -0700

    master: Add useful logging about the request

--- routes/index.js
+++ routes/index.js
@@ -15,12 +15,16 @@ const features = {
 }
 
 router.get('/test', async function(req, res, next) {
-  let response = await nlu.analyze(req.query.text, features);
+  let response = await nlu.analyze(req.query.text, features).catch((err) => {
+    console.log(`Error caught for /test: ${err}`);
+  });
   res.json(response);
 });
 
 router.post('/fileUpload', async function(req, res, next) {
-  let response = await nlu.analyze(req.files.file.data.toString('utf8'), features);
+  let response = await nlu.analyze(req.files.file.data.toString('utf8'), features).catch((err) => {
+    console.log(`Error caught while performing /fileUpload call: ${err}`);
+  });
   res.json(response);
 });
 
